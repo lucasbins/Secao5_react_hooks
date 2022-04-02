4cdcd15b2106a2a4da8373086c7611dc504bc84b
lucasbins <lucas.braga@rede.ulbra.br>
2022-04-01 21:53:34 -0300

    66. useRef #01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './index.css'
-import ReactDOM from 'react-dom'
+/*com o react 18 utilizar o react-dom/client ao
+inves do reactDOM para remover o aviso*/
+import * as ReactDOMClient from 'react-dom/client';
 import React from 'react'
 
 import App from './views/App'
+const container = document.getElementById('root');
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+const root = ReactDOMClient.createRoot(container);
+
+
+root.render(<App />);
